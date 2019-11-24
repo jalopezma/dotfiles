844e95ab4aad9ed1e8bfe46bf8452a4bd0f728c0
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2019-11-24 13:27:01 +0000

    setting up polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,5 +8,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+# Launch the same bar for each monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+done
