53e97556e87bf303582d0367e3e6dc5c42651fd5
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2022-01-13 14:33:23 +0100

    make yvpn take whatever .ovpn file find at home

--- scripts/yvpn.sh
+++ scripts/yvpn.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-CONFIG_FILE='/home/joselopez/yieldify_laptop_jose_lopez@yieldify_com@_[London].ovpn'
+CONFIG_FILE=$(ls -CN ~/*yieldify*.ovpn 2>/dev/null)
 USERNAME=$(whoami)
+
+if [ -z $CONFIG_FILE ]; then
+  echo "[yvpn] ovpn file not found in $HOME"
+  exit 1
+fi
 
 sessions=$(openvpn3 sessions-list)
 echo "$sessions"
 if [ "$sessions" = "No sessions available" ]; then
-  echo "> openvpn3 session-start --config "$CONFIG_FILE" "$USERNAME" &>/dev/null &"
+  echo "> openvpn3 session-start --config $CONFIG_FILE $USERNAME &>/dev/null &"
   $(openvpn3 session-start --config "$CONFIG_FILE" "$USERNAME" &>/dev/null &)
+  echo "[yvpn] Session started"
 else
   while IFS= read -r path; do
       echo "> openvpn3 session-manage --disconnect --path $path"
       $(openvpn3 session-manage --disconnect --path "$path" &>/dev/null &)
   done < <( openvpn3 sessions-list | grep 'Path:' | awk '{print $2}' )
+  echo "[yvpn] Sessions terminated"
 fi
