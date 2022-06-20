216091f3577b4eae8a09074fa56c79de2859a055
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2022-06-20 16:10:41 +0200

    chore: log output for openvpn3 to /tmp/yvpn-log

--- scripts/yvpn.sh
+++ scripts/yvpn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CONFIG_FILE=$(ls -CN ~/*yieldify*.ovpn 2>/dev/null)
 USERNAME=$(whoami)
+LOG_FILE="/tmp/yvpn-log"
 
 if [ -z $CONFIG_FILE ]; then
   echo "[yvpn] ovpn file not found in $HOME"
@@ -10,13 +11,13 @@ fi
 sessions=$(openvpn3 sessions-list)
 echo "$sessions"
 if [ "$sessions" = "No sessions available" ]; then
-  echo "> openvpn3 session-start --config $CONFIG_FILE $USERNAME &>/dev/null &"
-  $(openvpn3 session-start --config "$CONFIG_FILE" "$USERNAME" &>/dev/null &)
+  echo "> openvpn3 session-start --config $CONFIG_FILE $USERNAME 2>&1 >> $LOG_FILE &"
+  $(openvpn3 session-start --config "$CONFIG_FILE" "$USERNAME" 2>&1 >> $LOG_FILE &)
   echo "[yvpn] Session started"
 else
   while IFS= read -r path; do
-      echo "> openvpn3 session-manage --disconnect --path $path"
-      $(openvpn3 session-manage --disconnect --path "$path" &>/dev/null &)
+      echo "> openvpn3 session-manage --disconnect --path $path 2>&1 >> $LOG_FILE"
+      $(openvpn3 session-manage --disconnect --path "$path" 2>>&1 >> $LOG_FILE &)
   done < <( openvpn3 sessions-list | grep 'Path:' | awk '{print $2}' )
   echo "[yvpn] Sessions terminated"
 fi
