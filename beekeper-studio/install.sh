a5d5ade0a5015bb6d53e230ca50d94d9362adfef
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-01-13 14:54:44 +0100

    fix: use snap to install beekeper

--- beekeper-studio/install.sh
+++ beekeper-studio/install.sh
@@ -28,11 +28,5 @@ createSymlink() {
 logFile=$1
 verbose=$2
 
-print "[beekeper] Install beekeper GPG key"
-run "$(wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -)"
-
-print "[beekeper] Add repo to apt lists directory"
-run "$(echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list)"
-
-print "[beekeper] Update apt and install"
-run "$(sudo apt-get update && sudo apt-get install beekeeper-studio)"
+print "[beekeper] Install beekeeper-studio with snap"
+run "$(sudo snap install beekeeper-studio)"
