33265f831a1a9a4272ba95cb36fa2e196f978429
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-09-11 09:47:25 +0200

    add default setup for gitsigns

--- lazy.nvim/lua/plugins/gitsigns.lua
+++ lazy.nvim/lua/plugins/gitsigns.lua
@@ -1,1 +1,23 @@
-return { 'lewis6991/gitsigns.nvim', {} }
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+    end,
+  },
+}
+
