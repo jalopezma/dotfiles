36126880aa4b8bbe774a6409c4e814b6ddf9756a
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-09-14 10:09:47 +0200

    install fonts in ~/.local/share/fonts instead of ~/.fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,11 +2,11 @@
 # install UbuntuMono Nerd Font from https://www.nerdfonts.com/font-downloads
 
 echo "[fonts] Copy stored fonts"
-mkdir -p ~/.fonts
-cp ./fonts/*.ttf ~/.fonts/
+mkdir -p ~/.local/share/fonts
+cp ./fonts/*.ttf ~/.local/share/fonts/
 
 echo "[fonts] Download UbuntuMono font"
 wget -q -O /tmp/fonts.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/UbuntuMono.zip
-unzip -qq -o /tmp/fonts.zip -d ~/.fonts
+unzip -qq -o /tmp/fonts.zip -d ~/.local/share/fonts/
 fc-cache -fv &> /dev/null
 echo "[fonts] exit"
