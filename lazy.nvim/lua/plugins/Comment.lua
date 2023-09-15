85d80bf55f45b7a11eed62f66ccb42a3158088e6
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-09-15 09:42:51 +0200

    add keymaps description for Comments

--- lazy.nvim/lua/plugins/Comment.lua
+++ lazy.nvim/lua/plugins/Comment.lua
@@ -6,3 +6,20 @@ return {
     })
   end,
 }
+
+-- NORMAL mode
+--
+-- `gcc` - Toggles the current line using linewise comment
+-- `gbc` - Toggles the current line using blockwise comment
+-- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+-- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+-- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+-- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
+-- `gco` - Insert comment to the next line and enters INSERT mode
+-- `gcO` - Insert comment to the previous line and enters INSERT mode
+-- `gcA` - Insert comment to end of the current line and enters INSERT mode
+--
+-- VISUAL mode
+--
+-- `gc` - Toggles the region using linewise comment
+-- `gb` - Toggles the region using blockwise comment
