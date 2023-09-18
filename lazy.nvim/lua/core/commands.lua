fd2bc87912f0ce722474075fccda6084a1fb0d91
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-09-18 09:00:06 +0200

    add commads lua file; adds FormatJSON command

---
+++ lazy.nvim/lua/core/commands.lua
@@ -0,0 +1,9 @@
+-- :FormatJSON to use it. Requires python installed
+-- <line1> is the starting line of the command range (see ‘:help <line1>`)
+-- <line2> is the final line of the command range (see ‘:help <line2>`)
+-- ! here indicates this is a filter to apply to the selected text (see :help filter)
+vim.api.nvim_create_user_command(
+  'FormatJSON',
+  '<line1>,<line2>!python -m json.tool',
+  { range = true, desc = 'Format JSON files' }
+)
