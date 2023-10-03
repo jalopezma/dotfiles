dd0ac0daee74d5a2fbb819eb61ceeb33561e072b
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-10-03 07:47:47 +0200

    add options to bufferline. Trying to set an offset over explorer

--- lazy.nvim/lua/plugins/bufferline.lua
+++ lazy.nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,19 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require("bufferline").setup()
+    require("bufferline").setup({
+      options = {
+        --- Tried to add an offset so buffer tabs would appear after the explorer but didn't work
+        offsets = {
+          {
+            filetype = "Neo-tree",
+            text = "File Explorer",
+            text_align = "center",
+            separator = true
+          },
+        },
+      }
+    })
   end,
+  -- enabled = false,
 }
