86901ecbcec831dd1f28b51273ecc5ca46828e8c
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-10-05 09:32:42 +0200

    change find word under cursor from <leader>gj to <leader>gw(word)

--- lazy.nvim/lua/plugins/telescope.lua
+++ lazy.nvim/lua/plugins/telescope.lua
@@ -1,7 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
   --tag = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
   -- enabled = false,
   config = function()
     local actions = require('telescope.actions')
@@ -22,7 +24,7 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Search string' })
-    vim.keymap.set('n', '<leader>fj', builtin.grep_string, { desc = 'Search word under cursor' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Search word under cursor' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
   end,
 }
