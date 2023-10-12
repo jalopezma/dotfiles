0c83e2f4ea1313944526b25c4325de53063cb932
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-10-12 08:35:14 +0200

    define only keympas needed and make them load the hop plugin

--- lazy.nvim/lua/plugins/hop.lua
+++ lazy.nvim/lua/plugins/hop.lua
@@ -3,11 +3,14 @@ return {
   version = '*',
   opts = {},
   -- enabled = false,
-  config = function()
-    local hop = require('hop')
-    hop.setup()
-    local directions = require('hop.hint').HintDirection
+  keys = {
+    { '<leader>sw', '<cmd>HopWord<cr>', desc = 'Hop to word' },
+    { '<leader>sc', '<cmd>HopChar1<cr>', desc = 'Hop to a char' },
+  },
+  -- config = function()
+  --   local hop = require('hop').setup()
 
+    -- local directions = require('hop.hint').HintDirection
     -- Disabled for now as it doesn't work with ; and I might not want to have this anymore
     -- extends f/F/t/T search adding the motion if more than one ocurrence
     --[[ vim.keymap.set('', 'f', function()
@@ -25,9 +28,5 @@ return {
     vim.keymap.set('', 'T', function()
       hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
     end, { remap = true }) ]]
-
-    local map = vim.keymap.set
-    map("n", "<leader>sw", "<cmd>HopWord<cr>", { desc = "Hop to word" })
-    map("n", "<leader>sc", "<cmd>HopChar1<cr>", { desc = "Hop to a char" })
-  end,
+  -- end,
 }
