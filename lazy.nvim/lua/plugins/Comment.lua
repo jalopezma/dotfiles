959c5fe35564da8f0c6e19d30d6009769bbbc991
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-10-12 08:08:43 +0200

    Lazyload comments plugin on keympas and define only the ones to use

--- lazy.nvim/lua/plugins/Comment.lua
+++ lazy.nvim/lua/plugins/Comment.lua
@@ -1,5 +1,37 @@
 return {
   'numToStr/Comment.nvim',
+  -- Lazy load when keymappings are enabled
+  -- Also only enables the mapping defined here
+  keys = {
+    -- Toggle current line or with count
+    {
+      'gc',
+      function()
+        return vim.v.count == 0
+            and '<Plug>(comment_toggle_linewise_current)'
+            or '<Plug>(comment_toggle_linewise_count)'
+      end,
+      expr = true,
+      desc = 'Comment current line',
+    },
+
+    -- Toggle current block
+    {
+      'gb',
+      function()
+          return vim.v.count == 0
+              and '<Plug>(comment_toggle_blockwise_current)'
+              or '<Plug>(comment_toggle_blockwise_count)'
+      end,
+      expr = true,
+      desc = 'Comment current block',
+    },
+
+    -- Toggle line in VISUAL mode
+    { 'gc', '<Plug>(comment_toggle_linewise_visual)', mode = 'x', desc = 'Comment line visual' },
+    -- Toggle block in VISUAL mode
+    { 'gb', '<Plug>(comment_toggle_blockwise_visual)', mode = 'x', desc = 'Comment block line visual' },
+  },
   config = function()
     require('Comment').setup({
       sticky = true,
@@ -8,24 +40,6 @@ return {
       --- Function to call before (un)comment
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     })
-
-    -- Toggle current line or with count
-    vim.keymap.set('n', 'gc', function()
-        return vim.v.count == 0
-            and '<Plug>(comment_toggle_linewise_current)'
-            or '<Plug>(comment_toggle_linewise_count)'
-    end, { expr = true, desc = 'Comment current line' })
-
-    vim.keymap.set('n', 'gb', function()
-        return vim.v.count == 0
-            and '<Plug>(comment_toggle_blockwise_current)'
-            or '<Plug>(comment_toggle_blockwise_count)'
-    end, { expr = true, desc = 'Comment current block' })
-
-
-    -- Toggle in VISUAL mode
-    vim.keymap.set('x', 'gc', '<Plug>(comment_toggle_linewise_visual)', { desc = 'Comment line visual' })
-    vim.keymap.set('x', 'gb', '<Plug>(comment_toggle_blockwise_visual)', { desc = 'Comment block line visual' })
   end,
   -- enabled = false,
   dependencies = {
