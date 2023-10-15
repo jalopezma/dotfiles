08b44748330f53676965e362892d236b3d055dac
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-10-15 12:34:59 +0200

    Ensure treesiter some languages are always installed

--- lazy.nvim/lua/plugins/nvim-treesitter.lua
+++ lazy.nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,10 @@ return {
   config = function()
     require('nvim-treesitter.configs').setup {
       ensure_installed = {
-        'css', 'html', 'javascript', 'lua', 'python', 'tsx', 'typescript', 'vim',
+        'bash', 'cmake', 'css', 'csv', 'dockerfile', 'git_config', 'git_rebase', 'gitattributes', 'gitcommit',
+        'gitignore', 'go', 'gomod', 'gosum', 'gpg', 'groovy', 'haskell', 'html', 'http', 'javascript', 'jq', 'jsdoc',
+        'json', 'lua', 'luadoc', 'make', 'markdown', 'markdown_inline', 'nix', 'python', 'regex', 'requirements', 'scss',
+        'sql', 'ssh_config', 'tsv', 'tsx', 'typescript', 'vim', 'vimdoc', 'vim', 'xml', 'yaml'
       },
       highlight = {
         enable = true,
