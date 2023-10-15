16210eb30a2f4f19632e0d21e03d07267a982321
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2023-10-15 12:33:39 +0200

    Update install script and plugins

--- lazy.nvim/install.sh
+++ lazy.nvim/install.sh
@@ -9,23 +9,25 @@ curl -sLO https://github.com/neovim/neovim/releases/latest/download/nvim.appimag
 chmod u+x nvim.appimage
 
 echo "[nvim] Link nvim.desktop file"
-createSymlink ~/repos/dotfiles/nvim/nvim.desktop /usr/share/applications/nvim.desktop true
+createSymlink ~/repos/dotfiles/nvim/nvim.desktop /usr/share/applications/nvim.desktop
 
-dir=/usr/local/bin/nvim
-echo "[nvim] nvim moved to \"$dir\""
-sudo mv nvim.appimage $dir
+binDirectory=/usr/local/bin/nvim
+echo "[nvim] nvim moved to \"$binDirectory\""
+sudo mv nvim.appimage $binDirectory
 
 echo "[nvim] Link nvim folder on .config"
 createSymlink ~/repos/dotfiles/lazy.nvim ~/.config/nvim
 
 echo "[nvim] Create ~/.config/nvim/{backup_files,swap_files,undo_files}"
+mkdir -p ~/.config/nvim/{backup_files,swap_files,undo_files}/
 
 # Dependencies for [telescope](https://github.com/nvim-telescope/telescope.nvim#suggested-dependencies)
-echo "[nvim] install plugins dependencies"
+echo "[nvim] Install plugins dependencies"
 sudo apt-get install ripgrep fd-find -y &> /dev/null
 
 echo "[nvim] Link fdfind to be executed as fd"
 ln -s $(which fdfind) ~/.local/bin/fd
 
-# sync: Run install, clean and update
+echo "[nvim] Install Lazy nvim and plugins"
+# The bang in the command will make it wait until it's finished
 nvim --headless "+Lazy! sync" +qa
