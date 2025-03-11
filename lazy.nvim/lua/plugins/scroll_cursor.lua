6828126305dee7a7e912f0db7d345522e33cdfef
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2025-03-11 22:04:26 +0100

    add cursor and scroll animation

---
+++ lazy.nvim/lua/plugins/scroll_cursor.lua
@@ -0,0 +1,13 @@
+return {
+  -- Smooth scrolling
+  {
+    "karb94/neoscroll.nvim",
+    opts = {},
+  },
+
+  -- Animations for the cursor movements
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  },
+}
