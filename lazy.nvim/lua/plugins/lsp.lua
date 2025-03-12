38c21bf4349056d0077ca179dfdc3f421604f7b0
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2025-03-12 06:45:38 +0100

    rename lsp keymaps

--- lazy.nvim/lua/plugins/lsp.lua
+++ lazy.nvim/lua/plugins/lsp.lua
@@ -60,32 +60,32 @@ return {
     end,
   },
 
-  -- Beautfier for quicklist
+  -- Beautyfier for quicklist
   {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     -- enabled = false,
     cmd = 'Trouble',
     keys = {
-      { 'gr',         '<cmd>Trouble lsp_references<cr>',         desc = 'reference' },
-      { 'gd',         '<cmd>Trouble lsp_definitions<cr>',        desc = 'definition' },
-      { 'gs',         '<cmd>vert winc ]<cr>',                    desc = 'definition in a vertical split' },
-      { 'gt',         '<cmd>Trouble lsp_type_definitions<cr>',   desc = 'type definition' },
-      { 'gD',         '<cmd>Trouble diagnostics<cr>',            desc = 'document diagnostics' },
+      { 'gr',         '<cmd>Trouble lsp_references<cr>',         desc = 'Reference (Trouble)' },
+      { 'gd',         '<cmd>Trouble lsp_definitions<cr>',        desc = 'Definition' },
+      { 'gs',         '<cmd>vert winc ]<cr>',                    desc = 'Definition in a vertical split' },
+      { 'gt',         '<cmd>Trouble lsp_type_definitions<cr>',   desc = 'Type definition' },
+      { 'gD',         '<cmd>Trouble diagnostics toggle focus=true<cr>',     desc = 'Document diagnostics' },
 
       -- Move to the previous diagnostic in the current buffer. See :help vim.diagnostic.goto_prev().
-      { 'gp',         function() vim.diagnostic.goto_prev() end, desc = 'previous diagnostic' },
+      { 'gp',         function() vim.diagnostic.goto_prev() end, desc = 'Previous diagnostic' },
       -- Move to the next diagnostic. See :help vim.diagnostic.goto_next().
-      { 'gn',         function() vim.diagnostic.goto_next() end, desc = 'next diagnostic' },
+      { 'gn',         function() vim.diagnostic.goto_next() end, desc = 'Next diagnostic' },
 
       -- Renames all references to the symbol under the cursor. See :help vim.lsp.buf.rename().
-      { '<leader>rn', function() vim.lsp.buf.rename() end,       desc = 'rename' },
+      { '<leader>rn', function() vim.lsp.buf.rename() end,       desc = 'Rename' },
       -- Displays hover information about the symbol under the cursor in a floating window. See :help vim.lsp.buf.hover().
-      { '<leader>K',  function() vim.lsp.buf.hover() end,        desc = 'information' },
+      { '<leader>K',  function() vim.lsp.buf.hover() end,        desc = 'Information' },
       -- Selects a code action available at the current cursor position. See :help vim.lsp.buf.code_action().
-      { '<leader>a',  function() vim.lsp.buf.code_action() end,  desc = 'code actions' },
+      { '<leader>a',  function() vim.lsp.buf.code_action() end,  desc = 'Code actions' },
       -- <F3>: Format code in current buffer. See :help vim.lsp.buf.format().
-      { '<leader>fo', function() vim.lsp.buf.format() end,       desc = 'format file' },
+      { '<leader>fo', function() vim.lsp.buf.format() end,       desc = 'Format file' },
     },
     -- Needed to register the command
     opts = {},
