b06c064429c7ee4a2607a05dccef61e0f9e2793e
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2025-03-19 09:32:19 +0100

    update augment ia plugin config

--- lazy.nvim/lua/plugins/ia.lua
+++ lazy.nvim/lua/plugins/ia.lua
@@ -1,4 +1,15 @@
-vim.g.augment_workspace_folders = { '~/repos/dotfiles' }
+vim.g.augment_workspace_folders = {
+  '~/repos/dotfiles',
+  '~/repos/YieldifyLabs/beric',
+  '~/repos/YieldifyLabs/westeros',
+  '~/repos/YieldifyLabs/nightswatch-citadel-pipeline',
+  '~/repos/YieldifyLabs/data-transformation',
+  '~/repos/YieldifyLabs/greyworm',
+  '~/repos/YieldifyLabs/missandei',
+  '~/repos/YieldifyLabs/davos',
+  '~/repos/YieldifyLabs/airflow',
+  '~/repos/YieldifyLabs/ironbank',
+}
 -- If :Augment signin doesn't display the auth link, disable all plugins but this one and try again
 return {
   {
@@ -7,9 +18,9 @@ return {
       local wk = require("which-key")
       wk.add({
         { "<leader>i",  group = "IA" },
-        { "<leader>ic", "<cmd>Augment chat<cr>",        desc = "Chat",              mode = "n" },
-        { "<leader>in", "<cmd>Augment chat-new<cr>",    desc = "New chat",          mode = "n" },
-        { "<leader>it", "<cmd>Augment chat-toggle<cr>", desc = "Toggle chat",       mode = "n" },
+        { "<leader>ic", "<cmd>Augment chat<cr>",          desc = "Chat",              mode = { "n", "v" } },
+        { "<leader>in", "<cmd>Augment chat-new<cr>",      desc = "New chat",          mode = "n" },
+        { "<leader>it", "<cmd>Augment chat-toggle<cr>",   desc = "Toggle chat",       mode = "n" },
         { "<C-y>",      "<cmd>call augment#Accept()<cr>", desc = "Accept suggestion", mode = "i" },
       })
     end,
